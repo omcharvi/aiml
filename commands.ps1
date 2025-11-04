d92c90f75ea60754e82d085b2b742c0cf9c141ef
chitranjan <kchitranjan7@gmail.com>
2025-11-04 22:33:24 +0530

    smg type for reference

---
+++ commands.ps1
@@ -0,0 +1,8 @@
+# git status -- to check status of file or changes 
+git add .
+# .   -  adds all the files to local commit ready repo
+git commit -m "smg type for reference" 
+#- commits the changes to local repo 
+git push origin 
+#- pushes all committed changes to main branch in remote repo in github 
+
--- commands.py
+++
@@ -1,5 +0,0 @@
-# git status -- to check status of file or changes 
-# git add .   -  adds all the files to local commit ready repo
-# git commit -m "smg type for reference" - commits the changes to local repo 
-# git push origin - pushes all committed changes to main branch in remote repo in github 
-
